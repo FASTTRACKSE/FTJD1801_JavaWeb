752c48b6c7e131051ee228d294296c276590d6dc
ThanhCL <thanhcl@gmail.com>
2018-10-10 20:21:27 +0700

    update getimage servlet

---
+++ ThanhCL/SampleProject/src/sampleproject/controller/GetImage.java
@@ -0,0 +1,53 @@
+package sampleproject.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetImage
+ */
+@WebServlet("/getimage/*")
+public class GetImage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GetImage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String filename = URLDecoder.decode(request.getPathInfo().substring(1), "UTF-8");
+		File file = new File("/Users/thanhcl/hinhcuatui", filename);
+		response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+		response.setHeader("Content-Length", String.valueOf(file.length()));
+		response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+		Files.copy(file.toPath(), response.getOutputStream());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
