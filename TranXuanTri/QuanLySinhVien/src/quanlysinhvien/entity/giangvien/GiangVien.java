1f92d0dacd20246faf98339f23615bfa4bf222e4
tranxuantri <39622219+tranxuantri@users.noreply.github.com>
2018-10-24 19:53:44 +0700

    edit entity.giangvien

--- TranXuanTri/QuanLySinhVien/src/quanlysinhvien/entity/giangvien/GiangVien.java
+++ TranXuanTri/QuanLySinhVien/src/quanlysinhvien/entity/giangvien/GiangVien.java
@@ -1,5 +1,33 @@
 package quanlysinhvien.entity.giangvien;
 
-public interface GiangVien {
-	public String getThongTinGiangVien();
+public class GiangVien {
+	String hoTen;
+	String email;
+
+	public GiangVien() {
+
+	}
+
+	public GiangVien(String hoTen, String email) {
+		super();
+		this.hoTen = hoTen;
+		this.email = email;
+	}
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
--- TranXuanTri/QuanLySinhVien/src/quanlysinhvien/entity/monhoc/LapTrinhPHP.java
+++ TranXuanTri/QuanLySinhVien/src/quanlysinhvien/entity/monhoc/LapTrinhPHP.java
@@ -1,19 +1,33 @@
 package quanlysinhvien.entity.monhoc;
 
+import java.util.List;
+
 import quanlysinhvien.entity.giangvien.GiangVien;
 
 public class LapTrinhPHP implements MonHoc {
 	GiangVien giangVien;
-
+	List<GiangVien> dsTroGiang;
+	
+	public LapTrinhPHP() {
+		
+	}
 	public LapTrinhPHP(GiangVien giangVien) {
 		super();
 		this.giangVien = giangVien;
 	}
 
-	@Override
-	public String getThongTinMonHoc() {
-		// TODO Auto-generated method stub
-		return "Lập trình Web PHP & MySQL";
+	public LapTrinhPHP(GiangVien giangVien, List<GiangVien> dsTroGiang) {
+		super();
+		this.giangVien = giangVien;
+		this.dsTroGiang = dsTroGiang;
+	}
+
+	public List<GiangVien> getDsTroGiang() {
+		return dsTroGiang;
+	}
+
+	public void setDsTroGiang(List<GiangVien> dsTroGiang) {
+		this.dsTroGiang = dsTroGiang;
 	}
 
 	public GiangVien getGiangVien() {
@@ -27,6 +41,18 @@ public class LapTrinhPHP implements MonHoc {
 	@Override
 	public String getThongTinGiangVien() {
 		// TODO Auto-generated method stub
-		return giangVien.getThongTinGiangVien();
+		return giangVien.getHoTen() + " || Email: " + giangVien.getEmail();
+	}
+
+	@Override
+	public List<GiangVien> getDanhSachTroGiang() {
+		// TODO Auto-generated method stub
+		return dsTroGiang;
+	}
+
+	@Override
+	public String getThongTinMonHoc() {
+		// TODO Auto-generated method stub
+		return "Lập trình php";
 	}
 }
