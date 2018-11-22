5c2a316e0051e7850f3a7a99ab1e5b0662d76c7e
tranxuantri <39622219+tranxuantri@users.noreply.github.com>
2018-11-22 10:42:32 +0700

    hibernate example

---
+++ TranXuanTri/ExampleHibernate/src/hibernate/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package hibernate.dao;
+
+import java.util.List;
+
+import hibernate.enity.Employee;
+ 
+ 
+public interface EmployeeDao {
+ 
+    Employee findById(int id);
+ 
+    void saveEmployee(Employee employee);
+     
+    void deleteEmployeeBySsn(String ssn);
+     
+    List<Employee> findAllEmployees();
+ 
+    Employee findEmployeeBySsn(String ssn);
+ 
+}
