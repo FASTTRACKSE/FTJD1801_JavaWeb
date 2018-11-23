c9548ec75d5c9b5d816d703c23b1b4bab4581462
nguyenvantu98 <tuboy.lenam@gmail.com>
2018-11-23 20:30:28 +0700

    Suýt nữa thì =))

--- NguyenTu/sinhviencrud_springmvc/src/sinhvien/controllers/SinhVienControllers.java
+++ NguyenTu/sinhviencrud_springmvc/src/sinhvien/controllers/SinhVienControllers.java
@@ -1,14 +1,23 @@
 package sinhvien.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import sinhvien.beans.SinhVien;
@@ -17,56 +26,116 @@ import sinhvien.dao.SinhVienDao;
 @Controller
 public class SinhVienControllers {
 	@Autowired
-	SinhVienDao dao;// will inject dao from xml file
+	SinhVienDao dao;
+	String name = null;
+	private static final String UPLOAD_DIRECTORY = "E:\\upload";
 
-	/*
-	 * It displays a form to input data, here "command" is a reserved request
-	 * attribute which is used to display object data into form
-	 */
-	@RequestMapping("/svform")
+	@RequestMapping("/add")
 	public ModelAndView showform() {
 		return new ModelAndView("AddForm", "command", new SinhVien());
 	}
 
-	/*
-	 * It saves object into database. The @ModelAttribute puts request data into
-	 * model object. You need to mention RequestMethod.POST method because default
-	 * request is GET
-	 */
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public ModelAndView save(@ModelAttribute("sv") SinhVien sv) {
-		dao.save(sv);
-		return new ModelAndView("redirect:/viewsv");// will redirect to viewsv request mapping
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("sv") SinhVien sv, @RequestParam CommonsMultipartFile file) throws IOException {
+
+		if (!(sv.getTenSv().equals("") || sv.getNamSinh() == 0)) {
+			if (file != null) {
+				String filename = file.getOriginalFilename();
+				byte[] bytes = file.getBytes();
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File(UPLOAD_DIRECTORY + File.separator + filename)));
+				stream.write(bytes);
+				stream.flush();
+				stream.close();
+				sv.setImage(filename);
+			}
+			dao.add(sv);
+		}
+		return new ModelAndView("redirect:/");
 	}
 
-	/* It provides list of employees in model object */
-	@RequestMapping("/viewsv")
-	public ModelAndView viewsv() {
-		List<SinhVien> list = dao.getEmployees();
-		return new ModelAndView("ViewForm", "list", list);
+	@RequestMapping("/")
+	public ModelAndView list() {
+		return new ModelAndView("redirect:/paging/1");
 	}
 
-	/*
-	 * It displays object data into form for the given id. The @PathVariable puts
-	 * URL data into variable.
-	 */
-	@RequestMapping(value = "/editsv/{id}")
-	public ModelAndView edit(@PathVariable int id) {
-		SinhVien sv = dao.getSinhVienById(id);
-		return new ModelAndView("EditForm", "command", sv);
+	@RequestMapping(value = "/viewSearch", method = RequestMethod.POST)
+	public ModelAndView search(@RequestParam("searchValue") String searchValue) {
+		name = searchValue;
+		return new ModelAndView("redirect:/");
 	}
 
-	/* It updates model object. */
-	@RequestMapping(value = "/editsave", method = RequestMethod.POST)
-	public ModelAndView editsave(@ModelAttribute("sv") SinhVien sv) {
-		dao.update(sv);
-		return new ModelAndView("redirect:/viewsv");
+	@RequestMapping("/paging/{pageValue}")
+	public ModelAndView paging(@PathVariable int pageValue, Model model) {
+		int perPage = 4;
+		int nPage = 1;
+		int currentPage = 0;
+		double totalCount;
+		if (name == null) {
+			String hoten = "";
+			List<SinhVien> list = dao.countSinhVien(hoten);
+			totalCount = list.size();
+		} else {
+			List<SinhVien> list = dao.countSinhVien(name);
+			totalCount = list.size();
+		}
+		int totalPage = (int) Math.ceil(totalCount / perPage);
+
+		nPage = pageValue;
+		if (nPage > totalPage) {
+			nPage = totalPage;
+		}
+		if (nPage < 1) {
+			nPage = 1;
+		}
+		currentPage = (nPage - 1) * perPage;
+
+		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("crPage", nPage);
+
+		if (name == null) {
+			name = "";
+		}
+		model.addAttribute("nameValue", name);
+
+		List<SinhVien> list = dao.getSearchLimitSinhVien(currentPage, perPage, name);
+		return new ModelAndView("ListForm", "list", list);
 	}
 
-	/* It deletes record for the given id in URL and redirects to /viewsv */
-	@RequestMapping(value = "/deletesv/{id}", method = RequestMethod.GET)
+//	@RequestMapping("/viewsv")
+//	public ModelAndView viewsv() {
+//		List<SinhVien> list = dao.getEmployees();
+//		return new ModelAndView("ViewForm", "list", list);
+//	}
+	@RequestMapping(value = "/formedit/{id}")
+	public ModelAndView editsave(@PathVariable int id) {
+		SinhVien sinhvien = dao.getSinhVienById(id);
+		return new ModelAndView("EditForm", "command", sinhvien);
+	}
+	@RequestMapping(value = "/editsv",method = RequestMethod.POST)
+	public ModelAndView editsave(@ModelAttribute("sv") SinhVien sv, @RequestParam CommonsMultipartFile file,
+			HttpSession session) throws IOException {
+		if (!(sv.getTenSv().equals("") || sv.getNamSinh() == 0)) {
+			if (!file.isEmpty()) {
+				String filename = file.getOriginalFilename();
+				byte[] bytes = file.getBytes();
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File(UPLOAD_DIRECTORY + File.separator + filename)));
+				stream.write(bytes);
+				stream.flush();
+				stream.close();
+				sv.setImage(filename);
+			}
+			dao.update(sv);
+		}
+		return new ModelAndView("redirect:/");
+	}
+
+	
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public ModelAndView delete(@PathVariable int id) {
 		dao.delete(id);
-		return new ModelAndView("redirect:/viewsv");
+		return new ModelAndView("redirect:/");
 	}
 }
--- NguyenTu/sinhviencrud_springmvc/src/sinhvien/dao/SinhVienDao.java
+++ NguyenTu/sinhviencrud_springmvc/src/sinhvien/dao/SinhVienDao.java
@@ -9,53 +9,96 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-
-
 import sinhvien.beans.*;
 
 import sinhvien.beans.SinhVien;
 
 public class SinhVienDao {
 	JdbcTemplate template;
-	
-	public void setTemplate(JdbcTemplate template) {  
-	    this.template = template;  
-	}  
-	
-	public int save(SinhVien p){  
-	    String sql="insert into sinhvien(maSv,hoTen,namSinh,email,gioiTinh,diaChi,lop,image)  values("+p.getMaSv()+",'"+p.getTenSv()+"',"+p.getNamSinh()+",'"+p.getEmail()+"','"+p.getGioiTinh()+"','"+p.getDiaChi()+"','"+p.getLop()+"','"+p.getImage()+"')";  
-	    return template.update(sql);  
-	}  
-	public int update(SinhVien p){  
-	    String sql="update sinhvien set maSv='"+p.getMaSv()+"', hoTen="+p.getTenSv()+",  namSinh='"+p.getNamSinh()+"',email="+p.getEmail()+",gioiTinh="+p.getGioiTinh()+",diaChi="+p.getDiaChi()+",lop="+p.getLop()+",image="+p.getImage()+", where id="+p.getId()+"";  
-	    return template.update(sql);  
-	}  
-	public int delete(int id){  
-	    String sql="delete from sinhvien where id="+id+"";  
-	    return template.update(sql);  
-	}  
-	
-	public SinhVien getSinhVienById(int id){  
-	    String sql="select * from sinhvien where id=?";  
-	    return template.queryForObject(sql, new Object[]{id},new BeanPropertyRowMapper<SinhVien>(SinhVien.class));  
-	}  
 
-	
-	public List<SinhVien> getEmployees(){  
-	    return template.query("select * from sinhvien",new RowMapper<SinhVien>(){  
-	        public SinhVien mapRow(ResultSet rs, int row) throws SQLException {  
-	            SinhVien e=new SinhVien();  
-	            e.setId(rs.getInt(1));  
-	            e.setMaSv(rs.getInt(2));  
-	            e.setTenSv(rs.getString(3));  
-	            e.setNamSinh(rs.getInt(4));
-	            e.setEmail(rs.getString(5));
-	            e.setGioiTinh(rs.getString(6));
-	            e.setDiaChi(rs.getString(7));
-	            e.setLop(rs.getString(8));
-	            e.setImage(rs.getString(9));
-	            return e;  
-	        }  
-	    });  
-	}  
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+
+	public int add(SinhVien p) {
+		String sql = "insert into sinhvien(hoTen,namSinh,email,diaChi,lop,image)  values('" + p.getTenSv() + "',"
+				+ p.getNamSinh() + ",'" + p.getEmail() + "','" + p.getDiaChi() + "','" + p.getLop() + "','"
+				+ p.getImage() + "')";
+		return template.update(sql);
+	}
+
+	public int update(SinhVien p) {
+		String sql = "update sinhvien set  hoTen='" + p.getTenSv() + "',  namSinh=" + p.getNamSinh() + ",email='"
+				+ p.getEmail() + "',diaChi='" + p.getDiaChi() + "',lop='" + p.getLop() + "',image='" + p.getImage()
+				+ "' where id=" + p.getId() + "";
+		return template.update(sql);
+	}
+
+	public int delete(int id) {
+		String sql = "delete from sinhvien where id=" + id + "";
+		return template.update(sql);
+	}
+
+	public SinhVien getSinhVienById(int id) {
+		String sql = "select * from sinhvien where id=?";
+		return template.queryForObject(sql, new Object[] { id }, new BeanPropertyRowMapper<SinhVien>(SinhVien.class));
+	}
+
+	public List<SinhVien> getEmployees() {
+		return template.query("select * from sinhvien", new RowMapper<SinhVien>() {
+			public SinhVien mapRow(ResultSet rs, int row) throws SQLException {
+				SinhVien e = new SinhVien();
+				e.setId(rs.getInt(1));
+
+				e.setTenSv(rs.getString(2));
+				e.setNamSinh(rs.getInt(3));
+				e.setEmail(rs.getString(4));
+
+				e.setDiaChi(rs.getString(5));
+				e.setLop(rs.getString(6));
+				e.setImage(rs.getString(7));
+				return e;
+			}
+		});
+	}
+
+	public List<SinhVien> getSearchLimitSinhVien(int start, int total, String name) {
+		return template.query(
+				"SELECT * FROM sinhvien where hoTen like \'%" + name + "%\' limit " + start + "," + total + "",
+				new RowMapper<SinhVien>() {
+					public SinhVien mapRow(ResultSet rs, int row) throws SQLException {
+						
+						SinhVien e = new SinhVien();
+						e.setId(rs.getInt(1));
+
+						e.setTenSv(rs.getString(2));
+						e.setNamSinh(rs.getInt(3));
+						e.setEmail(rs.getString(4));
+
+						e.setDiaChi(rs.getString(5));
+						e.setLop(rs.getString(6));
+						e.setImage(rs.getString(7));
+						return e;
+					}
+				});
+	}
+
+	public List<SinhVien> countSinhVien(String name) {
+		return template.query("SELECT * FROM sinhvien where hoTen like \'%" + name + "%\'", new RowMapper<SinhVien>() {
+			public SinhVien mapRow(ResultSet rs, int row) throws SQLException {
+				SinhVien e = new SinhVien();
+				e.setId(rs.getInt(1));
+
+				e.setTenSv(rs.getString(2));
+				e.setNamSinh(rs.getInt(3));
+				e.setEmail(rs.getString(4));
+
+				e.setDiaChi(rs.getString(5));
+				e.setLop(rs.getString(6));
+				e.setImage(rs.getString(7));
+				return e;
+			}
+		});
+	}
+
 }
